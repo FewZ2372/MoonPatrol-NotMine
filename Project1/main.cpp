52a90bd80831c849b8acea37a87b6c8547dc3836
Leonardo <leo99brizuela@gmail.com>
2022-10-25 13:22:10 -0300

    salto

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,14 +4,38 @@
 using namespace std;
 const float SCREEN_WIDHT = 1000;
 const float SCREEN_HEIGHT = 800;
+
 int main()
 {
-	InitWindow((int)(SCREEN_HEIGHT),(int)(SCREEN_WIDHT) , "Moon Patrol");
+	InitWindow(static_cast<int>(SCREEN_HEIGHT), static_cast<int>(SCREEN_WIDHT), "Moon Patrol");
 
-	Rectangle caja={ (float)((GetScreenHeight())) /(float) 2,(float)((GetScreenWidth())) /(float) 2, 20, 20 };
-	while (!WindowShouldClose)
+	int centerScreenX = GetScreenWidth() / 2;
+	int centerScreenY = GetScreenHeight() / 2;
+
+	Rectangle caja= { static_cast<float>(centerScreenX), 
+					  static_cast<float>(centerScreenY),
+					  20.0f, 
+					  20.0f };
+
+	while (!WindowShouldClose())
 	{
-		DrawRectangle((int)((GetScreenWidth())) / (float)2, (int)((GetScreenHeight())) / (float)2, 20, 20, RED);
 
+		BeginDrawing();
+
+		if (IsKeyDown(KEY_SPACE))
+		{
+			DrawRectangle(static_cast<int>(GetScreenWidth()) / (2), static_cast<int>(GetScreenHeight()) / (2)-40, 20, 20, WHITE);
+
+		}
+		else
+		{
+			DrawRectangle(static_cast<int>(GetScreenWidth()) / (2), static_cast<int>(GetScreenHeight()) / (2), 20, 20, RED);
+		}
+
+		ClearBackground(BLACK);
+		EndDrawing();
 	}
+
+	CloseWindow();
+	return 0;
 }
